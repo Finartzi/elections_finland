62fa1d88beb50794b3e04a0d76299267a0220df5
artzi <arto.paeivinen@gmail.com>
2025-05-03 15:53:19 +0300

    Initial Commit ... nominators (parties) handling a bit forward

--- source/logic.py
+++ source/logic.py
@@ -7,29 +7,37 @@ import file_handling as handle
 import cls as c
 
 def get_datafiles():
-    #return os.listdir("JSON-data")
     return [f for f in pathlib.Path("JSON-data").glob("*.json")]
 
 def nominators():
     values = handle.getJSON("JSON-data/kv2025_nominators.json")
-    p.pprint(values["@identity-code"])
-    p.pprint(values["@election-type"])
-    #p.pprint(values["election"]["country-data"]["nominator"][1])
-    party = c.Nominators(values["election"]["country-data"]["nominator"][0]["@name"],
-                         values["election"]["country-data"]["nominator"][0]["@abbreviation"],
-                         values["election"]["country-data"]["nominator"][0]["@advance-votes"],
-                         values["election"]["country-data"]["nominator"][0]["@advance-votes-percent"],
-                         values["election"]["country-data"]["nominator"][0]["@election-day-votes"],
-                         values["election"]["country-data"]["nominator"][0]["@election-day-votes-percent"],
-                         values["election"]["country-data"]["nominator"][0]["@total-votes"],
-                         values["election"]["country-data"]["nominator"][0]["@total-votes-percent"],
-                         values["election"]["country-data"]["nominator"][0]["@seats"])
+    print("Alueita: " + str(values["election"]["country-data"]["area-data"]["@number-of-polling-districts"]))
+    """
+        for item in values["election"]["country-data"].keys():
+            print(item)
+    """
 
-    p.pprint(["Puolue: " + party.name, "Lyhenne: " + party.abbreviation,
-              "Ennakkoäänet: " + party.advance_votes, "Ennakko %: " + party.advance_votes_percent,
-             "Vaalipäivän äänet: " + party.election_day_votes, "Vaalipäivän %: " + party.advance_votes_percent,
-             "Äänet yhteensä: " + party.total_votes, "Äänet %: " + party.total_votes_percent,
-             "Paikkoja: " + party.seats])
+    parties = len(values["election"]["country-data"]["nominator"])
+    print("Puolueita: " + str(parties))
+    print("---")
+
+    for x in range(parties):
+        party = c.Nominators(values["election"]["country-data"]["nominator"][x]["@name"],
+                             values["election"]["country-data"]["nominator"][x]["@abbreviation"],
+                             values["election"]["country-data"]["nominator"][x]["@advance-votes"],
+                             values["election"]["country-data"]["nominator"][x]["@advance-votes-percent"],
+                             values["election"]["country-data"]["nominator"][x]["@election-day-votes"],
+                             values["election"]["country-data"]["nominator"][x]["@election-day-votes-percent"],
+                             values["election"]["country-data"]["nominator"][x]["@total-votes"],
+                             values["election"]["country-data"]["nominator"][x]["@total-votes-percent"],
+                             values["election"]["country-data"]["nominator"][x]["@seats"])
+
+        p.pprint(["Puolue: " + party.name, "Lyhenne: " + party.abbreviation,
+                  "Ennakkoäänet: " + party.advance_votes, "Ennakko %: " + party.advance_votes_percent,
+                 "Vaalipäivän äänet: " + party.election_day_votes, "Vaalipäivän %: " + party.advance_votes_percent,
+                 "Äänet yhteensä: " + party.total_votes, "Äänet %: " + party.total_votes_percent,
+                 "Paikkoja: " + party.seats])
+        print("---")
 
 
 def run():
