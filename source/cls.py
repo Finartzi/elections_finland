0c65a5d43137f42c9b64669810dfaacce2feab85
artzi <arto.paeivinen@gmail.com>
2025-05-04 18:57:07 +0300

    Initial Commit ... started with results, works. Missing "area-data" handling, otherwise ok.

--- source/cls.py
+++ source/cls.py
@@ -30,4 +30,82 @@ class ElectionCompare:
         self.seats = str(seats)
         self.seat_change = str(seat_change)
 
+class BasicVotingData:
+    def __init__(self, eligible_voters_total, eligible_voters_men, eligible_voters_women,
+                 eligible_voters_living_in_finland_total, eligible_voters_living_in_finland_men,
+                 eligible_voters_living_in_finland_women, advance_voting_turnout,
+                 election_day_voting_turnout, total_voting_turnout, number_to_be_elected):
+        self.eligible_voters_total = str(eligible_voters_total)
+        self.eligible_voters_men = str(eligible_voters_men)
+        self.eligible_voters_women = str(eligible_voters_women)
+        self.eligible_voters_living_in_finland_total = str(eligible_voters_living_in_finland_total)
+        self.eligible_voters_living_in_finland_men = str(eligible_voters_living_in_finland_men)
+        self.eligible_voters_living_in_finland_women = str(eligible_voters_living_in_finland_women)
+        self.advance_voting_turnout = str(advance_voting_turnout)
+        self.election_day_voting_turnout = str(election_day_voting_turnout)
+        self.total_voting_turnout = str(total_voting_turnout)
+        self.number_to_be_elected = str(number_to_be_elected)
+
+class BasisVotingDataCompare:
+    def __init__(self,
+                 election_event_name_abbreviation, eligible_voters_living_in_finland_total,
+                 advance_voting_turnout, election_day_voting_turnout, total_voting_turnout,
+                 approved_votes_total, invalid_votes_total, number_to_be_elected):
+        self.election_event_name_abbreviation = election_event_name_abbreviation
+        self.eligible_voters_living_in_finland_total = str(eligible_voters_living_in_finland_total)
+        self.advance_voting_turnout = str(advance_voting_turnout)
+        self.election_day_voting_turnout = str(election_day_voting_turnout)
+        self.total_voting_turnout = str(total_voting_turnout)
+        self.approved_votes_total = str(approved_votes_total)
+        self.invalid_votes_total = str(invalid_votes_total)
+        self.number_to_be_elected = str(number_to_be_elected)
+
+class BasicVotingDataSupplemental:
+    def __init__(self,
+                 advance_votes, advance_votes_males, election_day_votes, election_day_votes_males,
+                 total_votes, total_votes_males, advance_votes_living_in_finland,
+                 advance_votes_living_in_finland_males, election_day_votes_living_in_finland,
+                 election_day_votes_living_in_finland_males, total_votes_living_in_finland,
+                 total_votes_living_in_finland_males, advance_voting_turnout_living_in_finland,
+                 election_day_voting_turnout_living_in_finland, total_voting_turnout_living_in_finland,
+                 advance_votes_abroad, advance_votes_abroad_males, election_day_votes_abroad,
+                 election_day_votes_abroad_males, total_votes_abroad, total_votes_abroad_males,
+                 advance_voting_turnout_abroad, election_day_voting_turnout_abroad,
+                 total_voting_turnout_abroad, approved_advance_votes, approved_election_day_votes,
+                 approved_total_votes, invalid_advance_votes, invalid_election_day_votes, invalid_total_votes,
+                 calculation_status_percent_advance_votes, calculation_status_percent_election_day_votes,
+                 calculation_status_percent):
+        self.advance_votes = str(advance_votes)
+        self.advance_votes_males = str(advance_votes_males)
+        self.election_day_votes = str(election_day_votes)
+        self.election_day_votes_males = str(election_day_votes_males)
+        self.total_votes = str(total_votes)
+        self.total_votes_males = str(total_votes_males)
+        self.advance_votes_living_in_finland = str(advance_votes_living_in_finland)
+        self.advance_votes_living_in_finland_males = str(advance_votes_living_in_finland_males)
+        self.election_day_votes_living_in_finland = str(election_day_votes_living_in_finland)
+        self.election_day_votes_living_in_finland_males = str(election_day_votes_living_in_finland_males)
+        self.total_votes_living_in_finland = str(total_votes_living_in_finland)
+        self.total_votes_living_in_finland_males = str(total_votes_living_in_finland_males)
+        self.advance_voting_turnout_living_in_finland = str(advance_voting_turnout_living_in_finland)
+        self.election_day_voting_turnout_living_in_finland = str(election_day_voting_turnout_living_in_finland)
+        self.total_voting_turnout_living_in_finland = str(total_voting_turnout_living_in_finland)
+        self.advance_votes_abroad = str(advance_votes_abroad)
+        self.advance_votes_abroad_males = str(advance_votes_abroad_males)
+        self.election_day_votes_abroad = str(election_day_votes_abroad)
+        self.election_day_votes_abroad_males = str(election_day_votes_abroad_males)
+        self.total_votes_abroad = str(total_votes_abroad)
+        self.total_votes_abroad_males = str(total_votes_abroad_males)
+        self.advance_voting_turnout_abroad = str(advance_voting_turnout_abroad)
+        self.election_day_voting_turnout_abroad = str(election_day_voting_turnout_abroad)
+        self.total_voting_turnout_abroad = str(total_voting_turnout_abroad)
+        self.approved_advance_votes = str(approved_advance_votes)
+        self.approved_election_day_votes = str(approved_election_day_votes)
+        self.approved_total_votes = str(approved_total_votes)
+        self.invalid_advance_votes = str(invalid_advance_votes)
+        self.invalid_election_day_votes = str(invalid_election_day_votes)
+        self.invalid_total_votes = str(invalid_total_votes)
+        self.calculation_status_percent_advance_votes = str(calculation_status_percent_advance_votes)
+        self.calculation_status_percent_election_day_votes = str(calculation_status_percent_election_day_votes)
+        self.calculation_status_percent = str(calculation_status_percent)
 
--- source/logic.py
+++ source/logic.py
@@ -111,7 +111,164 @@ def nominators(data_file):
     print("---")
 
 
+def results(data_file):
+    def old_country_data(data, data_selection):
+        values = data
+        old = c.BasisVotingDataCompare(
+            values["election"]["country-data"]["basic-voting-data"]["comparison-election"][data_selection]["@election-event-name-abbreviation"],
+            values["election"]["country-data"]["basic-voting-data"]["comparison-election"][data_selection]["@eligible-voters-living-in-finland-total"],
+            values["election"]["country-data"]["basic-voting-data"]["comparison-election"][data_selection]["@advance-voting-turnout"],
+            values["election"]["country-data"]["basic-voting-data"]["comparison-election"][data_selection]["@election-day-voting-turnout"],
+            values["election"]["country-data"]["basic-voting-data"]["comparison-election"][data_selection]["@total-voting-turnout"],
+            values["election"]["country-data"]["basic-voting-data"]["comparison-election"][data_selection]["@approved-votes-total"],
+            values["election"]["country-data"]["basic-voting-data"]["comparison-election"][data_selection]["@invalid-votes-total"],
+            values["election"]["country-data"]["basic-voting-data"]["comparison-election"][data_selection]["@number-to-be-elected"])
+        value = [old.election_event_name_abbreviation,
+                 old.eligible_voters_living_in_finland_total,
+                 old.advance_voting_turnout,
+                 old.election_day_voting_turnout,
+                 old.total_voting_turnout,
+                 old.approved_votes_total,
+                 old.invalid_votes_total,
+                 old.number_to_be_elected]
+        return value
+
+    values = handle.getJSON(data_file)
+    print("Vaalipiirien lukumäärä: " + str(values["election"]["country-data"]["area-data"]["@number-of-polling-districts"]))
+    print(" - ")
+    voting = c.BasicVotingData(
+        values["election"]["country-data"]["basic-voting-data"]["@eligible-voters-total"],
+        values["election"]["country-data"]["basic-voting-data"]["@eligible-voters-men"],
+        values["election"]["country-data"]["basic-voting-data"]["@eligible-voters-women"],
+        values["election"]["country-data"]["basic-voting-data"]["@eligible-voters-living-in-finland-total"],
+        values["election"]["country-data"]["basic-voting-data"]["@eligible-voters-living-in-finland-men"],
+        values["election"]["country-data"]["basic-voting-data"]["@eligible-voters-living-in-finland-women"],
+        values["election"]["country-data"]["basic-voting-data"]["@advance-voting-turnout"],
+        values["election"]["country-data"]["basic-voting-data"]["@election-day-voting-turnout"],
+        values["election"]["country-data"]["basic-voting-data"]["@total-voting-turnout"],
+        values["election"]["country-data"]["basic-voting-data"]["@number-to-be-elected"])
+
+    supplemental = c.BasicVotingDataSupplemental(
+        values["election"]["country-data"]["supplemental-voting-data"]["@advance-votes"],
+        values["election"]["country-data"]["supplemental-voting-data"]["@advance-votes-males"],
+        values["election"]["country-data"]["supplemental-voting-data"]["@election-day-votes"],
+        values["election"]["country-data"]["supplemental-voting-data"]["@election-day-votes-males"],
+        values["election"]["country-data"]["supplemental-voting-data"]["@total-votes"],
+        values["election"]["country-data"]["supplemental-voting-data"]["@total-votes-males"],
+        values["election"]["country-data"]["supplemental-voting-data"]["@advance-votes-living-in-finland"],
+        values["election"]["country-data"]["supplemental-voting-data"]["@advance-votes-living-in-finland-males"],
+        values["election"]["country-data"]["supplemental-voting-data"]["@election-day-votes-living-in-finland"],
+        values["election"]["country-data"]["supplemental-voting-data"]["@election-day-votes-living-in-finland-males"],
+        values["election"]["country-data"]["supplemental-voting-data"]["@total-votes-living-in-finland"],
+        values["election"]["country-data"]["supplemental-voting-data"]["@total-votes-living-in-finland-males"],
+        values["election"]["country-data"]["supplemental-voting-data"]["@advance-voting-turnout-living-in-finland"],
+        values["election"]["country-data"]["supplemental-voting-data"]["@election-day-voting-turnout-living-in-finland"],
+        values["election"]["country-data"]["supplemental-voting-data"]["@total-voting-turnout-living-in-finland"],
+        values["election"]["country-data"]["supplemental-voting-data"]["@advance-votes-abroad"],
+        values["election"]["country-data"]["supplemental-voting-data"]["@advance-votes-abroad-males"],
+        values["election"]["country-data"]["supplemental-voting-data"]["@election-day-votes-abroad"],
+        values["election"]["country-data"]["supplemental-voting-data"]["@election-day-votes-abroad-males"],
+        values["election"]["country-data"]["supplemental-voting-data"]["@total-votes-abroad"],
+        values["election"]["country-data"]["supplemental-voting-data"]["@total-votes-abroad-males"],
+        values["election"]["country-data"]["supplemental-voting-data"]["@advance-voting-turnout-abroad"],
+        values["election"]["country-data"]["supplemental-voting-data"]["@election-day-voting-turnout-abroad"],
+        values["election"]["country-data"]["supplemental-voting-data"]["@total-voting-turnout-abroad"],
+        values["election"]["country-data"]["supplemental-voting-data"]["@approved-advance-votes"],
+        values["election"]["country-data"]["supplemental-voting-data"]["@approved-election-day-votes"],
+        values["election"]["country-data"]["supplemental-voting-data"]["@approved-total-votes"],
+        values["election"]["country-data"]["supplemental-voting-data"]["@invalid-advance-votes"],
+        values["election"]["country-data"]["supplemental-voting-data"]["@invalid-election-day-votes"],
+        values["election"]["country-data"]["supplemental-voting-data"]["@invalid-total-votes"],
+        values["election"]["country-data"]["supplemental-voting-data"]["@calculation-status-percent-advance-votes"],
+        values["election"]["country-data"]["supplemental-voting-data"]["@calculation-status-percent-election-day-votes"],
+        values["election"]["country-data"]["supplemental-voting-data"]["@calculation-status-percent"])
+
+    p.pprint(["Äänioikeutettuja: " + voting.eligible_voters_total + ", " +
+              old_country_data(values,0)[0] + ": " + old_country_data(values, 0)[1] + " " +
+              old_country_data(values, 1)[0] + ": " + old_country_data(values, 1)[1],
+                  ", joista miehiä: " + voting.eligible_voters_men,
+                  ", ja naisia: " + voting.eligible_voters_women,
+                  "Suomessa asuvia: " + voting.eligible_voters_living_in_finland_total,
+                  ", joista miehiä: " + voting.eligible_voters_living_in_finland_men,
+                  ", ja naisia: " + voting.eligible_voters_living_in_finland_women,
+                  "Ennakkoäänet %:" + voting.advance_voting_turnout + ", " +
+              old_country_data(values, 0)[0] + ": " + old_country_data(values, 0)[2] + " " +
+              old_country_data(values, 1)[0] + ": " + old_country_data(values, 1)[2],
+                  "Vaalipäivän äänet %:" + voting.election_day_voting_turnout + ", " +
+              old_country_data(values, 0)[0] + ": " + old_country_data(values, 0)[3] + " " +
+              old_country_data(values, 1)[0] + ": " + old_country_data(values, 1)[3],
+                  "Kokonais %: " + voting.total_voting_turnout + ", " +
+              old_country_data(values, 0)[0] + ": " + old_country_data(values, 0)[4] + " " +
+              old_country_data(values, 1)[0] + ": " + old_country_data(values, 1)[4],
+                  "Kokonaispaikkamäärä: " + voting.number_to_be_elected + ", " +
+              old_country_data(values, 0)[0] + ": " + old_country_data(values, 0)[7] + " " +
+              old_country_data(values, 1)[0] + ": " + old_country_data(values, 1)[7],
+              "Aiemmmissa vaaleissa annetuista äänistä ..",
+              "Hyväksyttyjä: " +
+              old_country_data(values, 0)[0] + ": " + old_country_data(values, 0)[5] + " " +
+              old_country_data(values, 1)[0] + ": " + old_country_data(values, 1)[5],
+              "Hylättyjä: "+
+              old_country_data(values, 0)[0] + ": " + old_country_data(values, 0)[6] + " " +
+              old_country_data(values, 1)[0] + ": " + old_country_data(values, 1)[6]])
+    print(" - - - ")
+
+    p.pprint(["Ennakkoääniä: " + supplemental.advance_votes +
+              ", joista miehiä: " + supplemental.advance_votes_males +
+              ", ja naisia: " + str(int(supplemental.advance_votes) - int(supplemental.advance_votes_males)),
+              "Äänestyspäivän äänet: " + supplemental.election_day_votes +
+              ", joista miehiä: " + supplemental.election_day_votes_males +
+              ", ja naisia: " + str(int(supplemental.election_day_votes) - int(supplemental.election_day_votes_males)),
+              "Kokonaisäänimäärä: " + supplemental.total_votes +
+              ", joista miehiä:" + supplemental.total_votes_males +
+              ", ja naisia: " + str(int(supplemental.total_votes) - int(supplemental.total_votes_males)),
+              "Suomessa asuvien ennakkoääniä: " + supplemental.advance_votes_living_in_finland +
+              ", joista miehiä: " + supplemental.advance_votes_living_in_finland_males +
+              ", ja naisia: " + str(int(supplemental.advance_votes_living_in_finland) - int(supplemental.advance_votes_living_in_finland_males)),
+              "Suomen ulkopuolelta ennakkoääniä: " + supplemental.advance_votes_abroad +
+              ", joista miehiä: " + supplemental.advance_votes_abroad_males +
+              ", ja naisia: " + str(int(supplemental.advance_votes_abroad) - int(supplemental.advance_votes_abroad_males)),
+              "Suomessa asuvien äänestyspäivän ääniä: " + supplemental.election_day_votes_living_in_finland +
+              ", joista miehiä: " + supplemental.election_day_votes_living_in_finland_males +
+              ", ja naisia: " + str(int(supplemental.election_day_votes_living_in_finland) - int(supplemental.election_day_votes_living_in_finland_males)),
+              "Suomessa ulkopuolelta äänestyspäivän ääniä: " + supplemental.election_day_votes_abroad +
+              ", joista miehiä: " + supplemental.election_day_votes_abroad_males +
+              ", ja naisia: " + str(int(supplemental.election_day_votes_abroad) - int(supplemental.election_day_votes_abroad_males)),
+              "Kaikkien Suomessa asuvien äänet: " + supplemental.total_votes_living_in_finland +
+              ", joista miehiä: " + supplemental.total_votes_living_in_finland_males +
+              ". ja naisia: " + str(int(supplemental.total_votes_living_in_finland) - int(supplemental.total_votes_living_in_finland_males)),
+              "Kaikkien Suomen ulkopuolella asuvien äänet: " + supplemental.total_votes_abroad +
+              ", joista miehiä: " + supplemental.total_votes_abroad_males +
+              ", ja naisia: " + str(int(supplemental.total_votes_abroad) - int(supplemental.total_votes_abroad_males)),
+              "Ennakkoäänestys % ulkomailla: " + supplemental.advance_voting_turnout_abroad,
+              "Äänestyspäivän % ulkomailla: " + supplemental.election_day_voting_turnout_abroad,
+              "Ulkomailta %: " + supplemental.total_voting_turnout_abroad,
+              "Ennakkoäänestys % kotimaassa: " + supplemental.advance_voting_turnout_living_in_finland,
+              "Äänestyspäivän % kotimaassa: " + supplemental.election_day_voting_turnout_living_in_finland,
+              "Kotimaassa %: " + supplemental.total_voting_turnout_living_in_finland,
+              "Hyväksytyt ennakkoäänet: " + supplemental.approved_advance_votes,
+              "Hyväksytyt äänestyspäivän äänet: " + supplemental.approved_election_day_votes,
+              "Hyväksytyt äänet: " + supplemental.approved_total_votes,
+              "Hylätyt ennakkoäänet: " + supplemental.invalid_advance_votes,
+              "Hylätyt äänestypäivän äänet: " + supplemental.invalid_election_day_votes,
+              "Hylätyt äänet: " + supplemental.invalid_total_votes,
+              "Ennakkoäänistä laskettu %: " + supplemental.calculation_status_percent_advance_votes,
+              "Äänestyspäivän äänistä laskettu %: " + supplemental.calculation_status_percent_election_day_votes,
+              "Äänistä laskettu %: " + supplemental.calculation_status_percent])
+    print(" - - - ")
+
+# values - keys (['@identity-code', '@election-type', 'election'])
+# Election -> (['@latest-snapshot-timestamp', '@calculation-phase', 'country-data', 'electoral-area'])
+# country-data -> (['@identifier', '@area-type', '@joined-area', 'area-data', 'basic-voting-data', 'supplemental-voting-data'])
+
+# p.pprint(values["election"]["country-data"]["basic-voting-data"]["comparison-election"].keys())
+#p.pprint(values["election"]["country-data"]["basic-voting-data"]["comparison-election"])
+
+
+
+
+
 def run():
     print(list_data_files[1])
     print(" --- ")
-    nominators(list_data_files[1])
+    #nominators(list_data_files[1])
+    results(list_data_files[2])
